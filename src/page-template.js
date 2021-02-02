3e3f1e5d08782f38ed4b84470c0c8e50009d77db
BrianCKWang <brian.ck.wang@gmail.com>
2021-02-02 17:17:06 -0500

    add page-template.js module

--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
 
 const generatePage = (name, github) => {
   return `
@@ -23,8 +19,5 @@ const generatePage = (name, github) => {
   `;
 };
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+module.exports = generatePage;
